0466974f33314b8579d47ae55950a87c1b7732f5
Alexx <103913006+Alexxjohnsams@users.noreply.github.com>
2023-09-14 09:47:17 +0100

    Update modal.blade.php

--- resources/views/pages/modal.blade.php
+++ resources/views/pages/modal.blade.php
@@ -145,3 +145,27 @@
     </div>
   </div>
 </div>
+
+
+
+
+<!-- delete modal --> 
+ <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> 
+     <div class="modal-dialog" role="document"> 
+       <div class="modal-content"> 
+         <div class="modal-header"> 
+           <h5 class="modal-title" id="myModalLabel">Delete </h5> 
+           <button type="button" class="close btn" data-dismiss="modal" aria-label="Close"> 
+             <span aria-hidden="true">&times;</span> 
+           </button> 
+         </div> 
+         <div class="modal-body"> 
+           <p>Proceed to delete?</p> 
+         </div> 
+         <div class="modal-footer"> 
+             <button type="button" id="deleteUserBtn" class="btn btn-danger">Yes</button> 
+             <button type="button" class="btn btn-primary" data-dismiss="modal">No</button>           
+         </div> 
+       </div> 
+     </div> 
+   </div>
